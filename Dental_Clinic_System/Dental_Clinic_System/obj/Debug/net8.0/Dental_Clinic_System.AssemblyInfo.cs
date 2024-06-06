31497c5c2617906ac4f8e0db824848c63462e61d
soyu <satori562003@gmail.com>
2024-06-07 00:58:21 +0700

    fix: Update content or code

--- Dental_Clinic_System/Dental_Clinic_System/obj/Debug/net8.0/Dental_Clinic_System.AssemblyInfo.cs
+++ Dental_Clinic_System/Dental_Clinic_System/obj/Debug/net8.0/Dental_Clinic_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dental_Clinic_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a880e30e620af072b30aa6b970bb458b06c751b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dental_Clinic_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dental_Clinic_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
