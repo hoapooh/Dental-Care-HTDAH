94954431ba38e9b120668a3ad3992c6a494e855d
soyu <satori562003@gmail.com>
2024-06-07 00:49:36 +0700

    feat: Add View Model for Local API

---
+++ Dental_Clinic_System/Dental_Clinic_System/ViewModels/APILocalVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Dental_Clinic_System.ViewModels
+{
+    public class APILocalVM
+    {
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string Ward { get; set; }
+    }
+}
