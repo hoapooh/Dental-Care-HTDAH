4352fcb05ac61b6927213f008b0133010a6cdd80
TDHanie <luongngocthuyduongvt@gmail.com>
2024-06-10 00:38:23 +0700

    Fix Area Admin
    - Chỉnh sửa Image của ManageSpecialty
    - Thêm thông báo lỗi khi Admin new Account
    - Chỉnh lại LayoutAdmin

--- Dental_Clinic_System/Dental_Clinic_System/Areas/Admin/ViewModels/ManagerSpecialtyVM.cs
+++ Dental_Clinic_System/Dental_Clinic_System/Areas/Admin/ViewModels/ManagerSpecialtyVM.cs
@@ -1,10 +1,15 @@
-﻿namespace Dental_Clinic_System.Areas.Admin.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dental_Clinic_System.Areas.Admin.ViewModels
 {
 	public class ManagerSpecialtyVM
 	{
 		public int Id { get; set; }
+
 		public string Name { get; set; } = null!;
-		public string? Image { get; set; }
+
+		public string Image { get; set; } = null!;
 		public string? Description { get; set; }
-	}
+        public IFormFile? ImageFile { get; set; }
+    }
 }
