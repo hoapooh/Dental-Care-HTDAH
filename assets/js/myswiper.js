6fb5af09964de9f8c8e625efafbbb3e6ab225e62
Hoapooh <hoapooh2912@gmail.com>
2024-06-25 17:26:30 +0700

    fix UI of clinic detail page and add Comment-Rating section at the bottom

--- assets/js/myswiper.js
+++ assets/js/myswiper.js
@@ -41,3 +41,25 @@ var swiper = new Swiper(".special__content", {
 		prevEl: ".swiper-button-prev",
 	},
 });
+
+var swiper = new Swiper(".reviews__content", {
+	slidesPerView: 2,
+	spaceBetween: 25,
+	loop: true,
+	centerSlide: "true",
+	fade: "true",
+	grabCursor: "true",
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		dynamicBullets: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
