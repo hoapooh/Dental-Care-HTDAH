570a2ac5cd2cfd4679e41723450536c41b79a75c
soyu <satori562003@gmail.com>
2024-07-11 04:55:26 +0700

    fix: change periodic calendar, logic and PDF for first dental treatment

---
+++ Dental_Clinic_System/Dental_Clinic_System/Areas/Dentist/Helper/DateOnlyConverter.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Dental_Clinic_System.Areas.Dentist.Helper
+{
+	public class DateOnlyConverter : JsonConverter<DateOnly>
+	{
+		private readonly string _format = "MM/dd/yyyy"; // This should match the format of your dates
+
+		public override void WriteJson(JsonWriter writer, DateOnly value, JsonSerializer serializer)
+		{
+			writer.WriteValue(value.ToString(_format));
+		}
+
+		public override DateOnly ReadJson(JsonReader reader, Type objectType, DateOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			return DateOnly.ParseExact((string)reader.Value, _format, null);
+		}
+	}
+
+}
