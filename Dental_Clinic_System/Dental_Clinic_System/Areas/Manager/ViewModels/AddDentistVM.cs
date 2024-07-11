aea9f3108243c52397ff5fbde8b0a41f629721e7
anhdtn2403 <anhdtn2403@gmail.com>
2024-07-12 01:01:24 +0700

    fix: VERIFY Dentistsp-Manager part

--- Dental_Clinic_System/Dental_Clinic_System/Areas/Manager/ViewModels/AddDentistVM.cs
+++ Dental_Clinic_System/Dental_Clinic_System/Areas/Manager/ViewModels/AddDentistVM.cs
@@ -5,19 +5,43 @@ namespace Dental_Clinic_System.Areas.Manager.ViewModels
 	public class AddDentistVM
 	{
 		public List<int> SpecialtyIDs { get; set; } = new List<int>();
+
+		[Required(ErrorMessage = "Vui lòng nhập tên đăng nhập!")]
+		[StringLength(30, ErrorMessage = "Tên đăng nhập không được vượt quá 30 ký tự.")]
+		[MinLength(3, ErrorMessage = "Tên đăng nhập phải có ít nhất 3 ký tự.")]
+		[RegularExpression("^[a-zA-Z][a-zA-Z0-9]*$", ErrorMessage = "Tên đăng nhập chỉ được chứa các ký tự chữ cái và số, và phải bắt đầu bằng chữ cái.")]
 		public string Username { get; set; }
 
+		[Required(ErrorMessage = "Vui lòng nhập mật khẩu!")]
+		[DataType(DataType.Password)]
+		[StringLength(30, ErrorMessage = "Mật khẩu không được vượt quá 30 ký tự")]
+		[MinLength(3, ErrorMessage = "Mật khẩu phải có ít nhất 3 ký tự")]
+		[RegularExpression("^[a-zA-Z][a-zA-Z0-9]*$", ErrorMessage = "Mật khẩu chỉ được chứa các ký tự chữ cái và số, và phải bắt đầu bằng chữ cái.")]
 		public string Password { get; set; }
 
-        public string FirstName { get; set; }
+		[Required(ErrorMessage = "Vui lòng nhập tên!")]
+		[RegularExpression(@"^[\p{L} ]*$", ErrorMessage = "Tên chỉ được chứa chữ cái và dấu cách!")]
+		public string FirstName { get; set; }
 
-        public string LastName { get; set; }
-        public string Gender { get; set; }
+		[Required(ErrorMessage = "Vui lòng nhập họ (và tên đệm)!")]
+		[RegularExpression(@"^[\p{L} ]*$", ErrorMessage = "Họ (và tên đệm) chỉ được chứa chữ cái và dấu cách!")]
+		public string LastName { get; set; }
 
-        public string Email { get; set; } = null!;
+		public string Gender { get; set; }
 
+		[Required(ErrorMessage = "Vui lòng nhập email!")]
+		[EmailAddress(ErrorMessage = "Email không hợp lệ.")]
+		[StringLength(50, ErrorMessage = "Email không được quá 50 ký tự.")]
+		[MinLength(3, ErrorMessage = "Email phải có ít nhất 3 ký tự.")]
+		public string Email { get; set; } = null!;
+
+		[Required(ErrorMessage = "Vui lòng nhập số điện thoại!")]
+		[Phone(ErrorMessage = "Số điện thoại không hợp lệ.")]
+		[RegularExpression(@"^\d{9,11}$", ErrorMessage = "Số điện thoại chỉ chứa các chữ số - độ dài từ 9 đến 11 ký tự .")]
 		public string PhoneNumber { get; set; }
-        public int DegreeID { get; set; }
+
+		public int DegreeID { get; set; }
+
         public string? Description { get; set; }
 
 
--- Dental_Clinic_System/Dental_Clinic_System/Areas/Manager/ViewModels/EditDentistVM.cs
+++ Dental_Clinic_System/Dental_Clinic_System/Areas/Manager/ViewModels/EditDentistVM.cs
@@ -8,9 +8,13 @@ namespace Dental_Clinic_System.Areas.Manager.ViewModels
 		public int DentistId { get; set; }
         public int AccountId { get; set; }
 
-        public string FirstName { get; set; }
+		[Required(ErrorMessage = "Vui lòng nhập tên!")]
+		[RegularExpression(@"^[\p{L} ]*$", ErrorMessage = "Tên chỉ được chứa chữ cái và dấu cách!")]
+		public string FirstName { get; set; }
 
-        public string LastName { get; set; }
+		[Required(ErrorMessage = "Vui lòng nhập họ (và tên đệm)!")]
+		[RegularExpression(@"^[\p{L} ]*$", ErrorMessage = "Họ (và tên đệm) chỉ được chứa chữ cái và dấu cách!")]
+		public string LastName { get; set; }
         public string Gender { get; set; }
         public int? Province { get; set; } = null!;
 
@@ -22,13 +26,23 @@ namespace Dental_Clinic_System.Areas.Manager.ViewModels
 
         public DateOnly? DateOfBirth { get; set; }
 
-        public string Email { get; set; } = null!;
+		[Required(ErrorMessage = "Vui lòng nhập email!")]
+		[EmailAddress(ErrorMessage = "Email không hợp lệ.")]
+		[StringLength(50, ErrorMessage = "Email không được quá 50 ký tự.")]
+		[MinLength(3, ErrorMessage = "Email phải có ít nhất 3 ký tự.")]
+		public string Email { get; set; } = null!;
 
+		[Required(ErrorMessage = "Vui lòng nhập số điện thoại!")]
+		[Phone(ErrorMessage = "Số điện thoại không hợp lệ.")]
+		[RegularExpression(@"^\d{9,11}$", ErrorMessage = "Số điện thoại chỉ chứa các chữ số - độ dài từ 9 đến 11 ký tự .")]
 		public string PhoneNumber { get; set; }
         public int DegreeID { get; set; }
         public string? Description { get; set; }
 
         public string Status { get; set; }
-        public string Image {  get; set; }
+
+		public string? Image {  get; set; }
+
+		public string? NewPassWord { get; set; }
     }
 }
