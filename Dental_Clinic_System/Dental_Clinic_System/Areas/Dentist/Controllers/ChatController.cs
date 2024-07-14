da1475ce1ca0451a5a2913bf5b741820a1511115
Okasu007 <Rivinger7@gmail.com>
2024-07-15 02:39:19 +0700

    feat: load conversation history and chat list for dentist

--- Dental_Clinic_System/Dental_Clinic_System/Areas/Dentist/Controllers/ChatController.cs
+++ Dental_Clinic_System/Dental_Clinic_System/Areas/Dentist/Controllers/ChatController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Dental_Clinic_System.Models.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dental_Clinic_System.Areas.Dentist.Controllers
 {
@@ -7,9 +9,55 @@ namespace Dental_Clinic_System.Areas.Dentist.Controllers
     [Authorize(AuthenticationSchemes = "DentistScheme", Roles = "Nha Sĩ")]
     public class ChatController : Controller
     {
-        public IActionResult ChatWithPatient()
-        {
+        private readonly DentalClinicDbContext _context;
 
+        public ChatController(DentalClinicDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> ChatDetail(int patientID)
+        {
+            var dentistID = HttpContext.Session.GetInt32("dentistID");
+
+            var messages = await _context.ChatHubMessages
+                .Where(m => (m.SenderId == dentistID && m.ReceiverId == patientID) || (m.SenderId == patientID && m.ReceiverId == dentistID))
+                .OrderBy(m => m.Timestamp)
+                .ToListAsync();
+
+            ViewBag.PatientID = patientID;
+            ViewBag.Messages = messages;
+
+            var patient = await _context.Accounts.FirstAsync(a => a.ID == patientID);
+
+            var patientName = $"{patient.FirstName} {patient.LastName}";
+
+            ViewBag.PatientName = patientName ?? "Ẩn Danh";
+
+            return View();
+        }
+
+        public async Task<IActionResult> ChatList()
+        {
+            var dentistID = HttpContext.Session.GetInt32("dentistID");
+
+            var chats = await _context.ChatHubMessages
+                .Where(m => m.SenderId == dentistID || m.ReceiverId == dentistID)
+                .Select(m => new
+                {
+                    Id = m.ID,
+                    Content = m.Content,
+                    Timestamp = m.Timestamp,
+                    SenderId = m.SenderId,
+                    ReceiverId = m.ReceiverId,
+                    SenderName = m.Sender.FirstName ?? "Ẩn" + " " + m.Sender.LastName ?? "Danh",
+                    ReceiverName = m.Receiver.FirstName ?? "Ẩn" + " " + m.Receiver.LastName ?? "Danh"
+                })
+                .GroupBy(m => m.SenderId == dentistID ? m.ReceiverId : m.SenderId)
+                .Select(g => g.OrderByDescending(m => m.Timestamp).FirstOrDefault())
+                .ToListAsync();
+
+            ViewBag.ChatList = chats;
             return View();
         }
     }
