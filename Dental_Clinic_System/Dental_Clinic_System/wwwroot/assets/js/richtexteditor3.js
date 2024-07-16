e7215be3bc79f2391f28c4b7f131ee52c823f51a
Hoapooh <hoapooh2912@gmail.com>
2024-07-16 11:37:15 +0700

    Update Chat UI, optimize website, fix filter of ClinicDentist
    - Delete unnecessary stuffs at the header and body of each page in the Customer View page
    - Change color of 'Tiếp tục' button in ClinicDentist and also fix filter method so that it working properly
    - Fix UI of Login page so that it won't break when the customer with small pc or laptop resolution acess to that page, and change the quote of register page to be Vietnamese

--- Dental_Clinic_System/Dental_Clinic_System/wwwroot/assets/js/richtexteditor3.js
+++ Dental_Clinic_System/Dental_Clinic_System/wwwroot/assets/js/richtexteditor3.js
@@ -1,7 +1,9 @@
 ﻿import {
 	ClassicEditor,
 	AccessibilityHelp,
+	Alignment,
 	Autosave,
+	BlockQuote,
 	Bold,
 	CKBox,
 	CKBoxImageEdit,
@@ -11,18 +13,28 @@
 	FontColor,
 	FontFamily,
 	FontSize,
+	Heading,
+	Highlight,
+	HorizontalLine,
 	ImageBlock,
 	ImageInsert,
 	ImageInsertViaUrl,
-	ImageResize,
 	ImageToolbar,
 	ImageUpload,
 	Italic,
 	Link,
 	List,
+	PageBreak,
 	Paragraph,
 	PictureEditing,
 	SelectAll,
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText,
 	Undo
 } from 'ckeditor5';
 
@@ -30,7 +42,7 @@
  * Please update the following values with your actual tokens.
  * Instructions on how to obtain them: https://ckeditor.com/docs/trial/latest/guides/real-time/quick-start.html
  */
-const CKBOX_TOKEN_URL = '<YOUR_CKBOX_TOKEN_URL>';
+const CKBOX_TOKEN_URL = 'https://113495.cke-cs.com/token/dev/b7920c906b38da8586a530b7b3bbebbf84e11e3052b706c7ce4d7f341398?limit=10';
 
 const editorConfig = {
 	toolbar: {
@@ -40,6 +52,8 @@ const editorConfig = {
 			'|',
 			'selectAll',
 			'|',
+			'heading',
+			'|',
 			'fontSize',
 			'fontFamily',
 			'fontColor',
@@ -48,9 +62,17 @@ const editorConfig = {
 			'bold',
 			'italic',
 			'|',
+			'specialCharacters',
+			'horizontalLine',
+			'pageBreak',
 			'link',
 			'insertImage',
+			'insertImageViaUrl',
 			'ckbox',
+			'highlight',
+			'blockQuote',
+			'|',
+			'alignment',
 			'|',
 			'bulletedList',
 			'numberedList',
@@ -61,7 +83,9 @@ const editorConfig = {
 	},
 	plugins: [
 		AccessibilityHelp,
+		Alignment,
 		Autosave,
+		BlockQuote,
 		Bold,
 		CKBox,
 		CKBoxImageEdit,
@@ -71,18 +95,28 @@ const editorConfig = {
 		FontColor,
 		FontFamily,
 		FontSize,
+		Heading,
+		Highlight,
+		HorizontalLine,
 		ImageBlock,
 		ImageInsert,
 		ImageInsertViaUrl,
-		ImageResize,
 		ImageToolbar,
 		ImageUpload,
 		Italic,
 		Link,
 		List,
+		PageBreak,
 		Paragraph,
 		PictureEditing,
 		SelectAll,
+		SpecialCharacters,
+		SpecialCharactersArrows,
+		SpecialCharactersCurrency,
+		SpecialCharactersEssentials,
+		SpecialCharactersLatin,
+		SpecialCharactersMathematical,
+		SpecialCharactersText,
 		Undo
 	],
 	ckbox: {
@@ -92,11 +126,56 @@ const editorConfig = {
 		supportAllValues: true
 	},
 	fontSize: {
-		options: [10, 12, 14, 'default', 18, 20, 22],
+		options: ['default', 12, 14, 16, 18, 20, 22],
 		supportAllValues: true
 	},
+	heading: {
+		options: [
+			{
+				model: 'paragraph',
+				title: 'Paragraph',
+				class: 'ck-heading_paragraph'
+			},
+			{
+				model: 'heading1',
+				view: 'h1',
+				title: 'Heading 1',
+				class: 'ck-heading_heading1'
+			},
+			{
+				model: 'heading2',
+				view: 'h2',
+				title: 'Heading 2',
+				class: 'ck-heading_heading2'
+			},
+			{
+				model: 'heading3',
+				view: 'h3',
+				title: 'Heading 3',
+				class: 'ck-heading_heading3'
+			},
+			{
+				model: 'heading4',
+				view: 'h4',
+				title: 'Heading 4',
+				class: 'ck-heading_heading4'
+			},
+			{
+				model: 'heading5',
+				view: 'h5',
+				title: 'Heading 5',
+				class: 'ck-heading_heading5'
+			},
+			{
+				model: 'heading6',
+				view: 'h6',
+				title: 'Heading 6',
+				class: 'ck-heading_heading6'
+			}
+		]
+	},
 	image: {
-		toolbar: ['imageTextAlternative', '|', 'resizeImage', '|', 'ckboxImageEdit']
+		toolbar: ['imageTextAlternative', '|', 'ckboxImageEdit']
 	},
 	initialData:
 		'<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n    You\'ve successfully created a CKEditor 5 project. This powerful text editor will enhance your application, enabling rich text editing\n    capabilities that are customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n    <li>\n        <strong>Integrate into your app</strong>: time to bring the editing into your application. Take the code you created and add to your\n        application.\n    </li>\n    <li>\n        <strong>Explore features:</strong> Experiment with different plugins and toolbar options to discover what works best for your needs.\n    </li>\n    <li>\n        <strong>Customize your editor:</strong> Tailor the editor\'s configuration to match your application\'s style and requirements. Or even\n        write your plugin!\n    </li>\n</ol>\n<p>\n    Keep experimenting, and don\'t hesitate to push the boundaries of what you can achieve with CKEditor 5. Your feedback is invaluable to us\n    as we strive to improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n    <li>📝 <a href="https://orders.ckeditor.com/trial/premium-features">Trial sign up</a>,</li>\n    <li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n    <li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n    <li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n    <li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n    See this text, but the editor is not starting up? Check the browser\'s console for clues and guidance. It may be related to an incorrect\n    license key if you use premium features or another feature-related requirement. If you cannot make it work, file a GitHub issue, and we\n    will help as soon as possible!\n</p>\n',
@@ -112,6 +191,9 @@ const editorConfig = {
 				}
 			}
 		}
+	},
+	menuBar: {
+		isVisible: true
 	},
 	placeholder: 'Type or paste your content here!'
 };
@@ -140,23 +222,4 @@ function configUpdateAlert(config) {
 
 		return true;
 	};
-
-	const valuesToUpdate = [];
-
-	configUpdateAlert.configUpdateAlertShown = true;
-
-	if (!isModifiedByUser(config.ckbox?.tokenUrl, '<YOUR_CKBOX_TOKEN_URL>')) {
-		valuesToUpdate.push('CKBOX_TOKEN_URL');
-	}
-
-	if (valuesToUpdate.length) {
-		window.alert(
-			[
-				'Please update the following values in your editor config',
-				'in order to receive full access to the Premium Features:',
-				'',
-				...valuesToUpdate.map(value => ` - ${value}`)
-			].join('\n')
-		);
-	}
 }
