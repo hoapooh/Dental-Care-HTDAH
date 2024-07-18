f7edf1a05a50684f60cf0697694b6703e0540f86
Hoapooh <hoapooh2912@gmail.com>
2024-07-18 13:34:54 +0700

    Updaet UI, add meta tag, add marquee
    - Update UI of Login and Register page
    - add meta tag descrition so that SEO works greatly
    - add marquee to specific page related to the clinic and patientrecord
    - fix visible password of Login and Register page
    - add back button at clinic specialty page

---
+++ Dental_Clinic_System/Dental_Clinic_System/wwwroot/assets/js/togglepassword.js
@@ -0,0 +1,21 @@
+﻿function togglePasswordVisibility(passwordGroup) {
+	var passwordInput = document.querySelector(
+		"." + passwordGroup + " .passwordInput"
+	);
+	var eyeIcon = document.querySelector(
+		"." + passwordGroup + " .eyeIcon"
+	);
+	var eyeSlashIcon = document.querySelector(
+		"." + passwordGroup + " .eyeSlashIcon"
+	);
+
+	if (passwordInput.type === "password") {
+		passwordInput.type = "text";
+		eyeIcon.style.display = "none";
+		eyeSlashIcon.style.display = "inline-block";
+	} else {
+		passwordInput.type = "password";
+		eyeIcon.style.display = "inline-block";
+		eyeSlashIcon.style.display = "none";
+	}
+}
